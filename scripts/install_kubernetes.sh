646205f7e25808083394b1ee88eec0ea37684a26
brianbianco <brian.bianco@gmail.com>
2016-01-29 14:15:02 -0500

    Starting to build up kubernetes install script

---
+++ scripts/install_kubernetes.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+apt-get install bridge-utils
+
+K8_V=v1.1.4
+
+wget https://github.com/kubernetes/kubernetes/releases/download/$K8_V/kubernetes.tar.gz
+tar zxvf kubernetes.tar.gz
+cd kubernetes/server
+tar zxvf kubernetes-server-linux-amd64.tar.gz
+cp kubernetes/server/bin/kubelet /usr/local/bin/
+cp kubernetes/server/bin/kube-proxy /usr/local/bin/
+cp kubernetes/server/bin/hyperkube /usr/local/bin/
+
+ETCD_V=$(cat ../cluster/images/etcd/Makefile | grep 'ETCD_VERSION =' | cut -f3 -d' ')
+
+HYPERKUBE_IMAGE=gcr.io/google_containers/hyperkube:$K8_V
+ETCD_IMAGE=gcr.io/google_containers/etcd:$ETCD_V
+
+docker pull $HYPERKUBE_IMAGE
+docker pull $ETCD_IMAGE
+
+mkdir -p /etc/kubernetes/manifests
+chown -R vagrant /etc/kubernetes
